5024445122ff68cb3cd7df0e7e1701c02e9ad1c5
kushmih <kushmih@gmail.com>
2025-10-08 21:41:12 +0300

    homework.lesson4

---
+++ lesson4/src/loop.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 9; i++) {
+    console.log('Ітератор від 0 до 9:', i);
+}
+
+console.log(' --- ');
+
+for (let j = 100; j >= 0; j -= 10) {
+    console.log('Ітератор від 100 до 0:', j);
+}
---
+++ lesson4/src/objects.js
@@ -0,0 +1,23 @@
+const dog = {
+    name: 'Хантер',
+    age: 10,
+    breed: 'Джек Рассел тер\'єр',
+    owner: {
+        name: 'Михайло',
+        age: 35
+    },
+    hobbies: ['сваритись на інших собак', 'нищити іграшки', 'гавкати на людей за дверима'],
+    bark: 'Гав-гав'
+};
+
+console.log(dog.bark, ' - каже пес');
+console.log('Хобі дурнуватого пса:', dog.hobbies);
+console.log('За все це відповідає', dog.owner.name);
+
+const trainedDog = structuredClone(dog);
+trainedDog.hobbies.push('приносити тапки');
+trainedDog.hobbies = trainedDog.hobbies.filter(
+    hobby => !['сваритись на інших собак', 'гавкати на людей за дверима'].includes(hobby)
+);
+
+console.log('Хобі тренованого пса:', trainedDog.hobbies);
