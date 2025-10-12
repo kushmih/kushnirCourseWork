5687b43124064639c749c27f2e12c0485eba1d88
kushmih <kushmih@gmail.com>
2025-10-12 14:09:56 +0300

    Lesson6 structure & promise.js

---
+++ lesson6/src/promises.js
@@ -0,0 +1,17 @@
+// promises.js
+function getData(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log('Отримані дані:', data);
+            return processData(data);
+        })
+        .then(summary => console.log('Результат обробки:', summary))
+        .catch(error => console.error('Помилка:', error));
+}
+
+function processData(data) {
+    return `Усього записів: ${data.length}`;
+}
+
+getData('https://jsonplaceholder.typicode.com/posts');
